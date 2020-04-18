4268e00f364edc533138e8f008c9d4f5659ece41
Thomas Adel <43293944+thomas545@users.noreply.github.com>
2020-04-18 15:40:19 +0200

    Update settings.py

--- pharmarose/settings.py
+++ pharmarose/settings.py
@@ -145,13 +145,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'statics')]
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = '/root/pharma-rose/static'
-MEDIA_ROOT ='/root/pharma-rose/media'
+# STATIC_ROOT = '/root/pharma-rose/static'
+# MEDIA_ROOT ='/root/pharma-rose/media'
 
 
 
