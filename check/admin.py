0c42eb616db059e64857d3e9fca04ea681b31dfa
thomas434 <thomas@dokkanz.com>
2020-07-30 16:36:24 +0200

    add import export

--- check/admin.py
+++ check/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
 from .models import Checking
 from django.contrib.auth.models import User, Group
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 
 
-class CheckingAdmin(admin.ModelAdmin):
+
+class CheckingResource(resources.ModelResource):
+    class Meta:
+        model = Checking
+        fields = ('product', 'bar_code', 'serial_number',)
+
+class CheckingAdmin(ImportExportModelAdmin):
+    resource_class = CheckingResource
     list_display = ('product', 'serial_number', 'bar_code',)
     list_filter = ('product',)
     search_fields = ('product',)
-
 
 
 
