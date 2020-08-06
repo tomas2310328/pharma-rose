1062404ab0b622db82f7975d7b410bfa9dbea1d1
thomas434 <thomas@dokkanz.com>
2020-08-06 16:24:09 +0200

    add changes

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import category_product_list, ProductListView, ProductDetailView
+from .views import category_product_list, ProductListView, ProductDetailView, SearchProduct
 # create urls here
 
 
@@ -11,8 +11,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
+    path('results/', SearchProduct.as_view(), name='search_results'),
     path('<slug:slug>/', category_product_list, name='category_product_list'),
     path('single-product/<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
-
-
 ]
---
+++ static/base.js
@@ -0,0 +1,12 @@
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 2000,
+    spaceBetween: 0,
+    grabCursor: true,
+    autoplay: {
+        autoplay: true,
+        delay: 2000, disableOnInteraction: false
+    }
+});
+
+
+
