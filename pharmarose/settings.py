e58ac2444f00d8e67eb7c930907a5ffce1388924
Thomas Saied <tomas.temo77@gmail.com>
2023-12-02 13:57:59 +0200

    updates

--- pharmarose/settings.py
+++ pharmarose/settings.py
@@ -70,24 +70,24 @@ WSGI_APPLICATION = 'pharmarose.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# for production #
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pharmarosedb',
-        'USER': 'eslam',
-        'PASSWORD': 'eslam123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# for production #
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'pharmarosedb',
+#         'USER': 'eslam',
+#         'PASSWORD': 'eslam123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
